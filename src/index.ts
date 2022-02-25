d656aea212df10ee947661635345a0df94adb1c6
Enrico <enrico.bottani@hotmail.com>
2022-02-25 09:17:30 +0100

    Namespace test #1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-export class KISAClient {
-    printVersion() {
-        console.log("v.1-snapshot")
+import KUser from "./model/User";
+
+
+export namespace KISAClient {
+
+    export class Version {
+        printVersion() {
+            console.log("v.0.0.2-snapshot")
+        }
     }
+    export type User = KUser;
 }
-export * from "./model/User"
